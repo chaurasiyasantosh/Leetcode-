ff9dea91257c166de8d7eb7ee48312bf9d8ddd9a
Santosh Chaurasiya <128207832+chaurasiyasantosh@users.noreply.github.com>
2023-06-21 19:34:01 +0530

    Create 2239. Find Closest Number to Zero.java

---
+++ 2239. Find Closest Number to Zero.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findClosestNumber(int[] nums) {
+        int min = Integer.MAX_VALUE;
+        int ans=0;
+        for(int i=0;i<nums.length;i++){
+            if(Math.abs(nums[i])< min){
+                min= Math.abs(nums[i]);
+                ans= nums[i];
+            }else if(Math.abs(nums[i])== min && nums[i]>ans){
+                ans= nums[i];
+            }
+        }
+          return ans;
+    }
+}
